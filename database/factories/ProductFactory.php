c7321b24b3bf93cc7fadcfa8c491c87b03ac839e
jordanye0 <jordanyeo.work@gmail.com>
2019-11-05 17:40:00 +0100

    products factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,9 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->sentence(10),
+        'name' => $faker->sentence(5),
         'image' => 'uploads/products/book.png',
-        'price' => 5000,
+        'price' => $faker->numberBetween(1000, 10000),
         'description' => $faker->paragraphs(5, true)
     ];
 });
