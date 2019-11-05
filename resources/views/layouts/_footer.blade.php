8a431aec4830868d1ec979b1afc655b402663893
jordanye0 <jordanyeo.work@gmail.com>
2019-11-05 15:51:28 +0100

    created layout for views

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,25 @@
+<footer class="footer">
+    <div class="sub-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/crum-mega-menu.js"></script>
+<script src="js/swiper.jquery.min.js"></script>
+<script src="js/theme-plugins.js"></script>
+<script src="js/main.js"></script>
+<script src="js/form-actions.js"></script>
+
+<script src="js/velocity.min.js"></script>
+<script src="js/ScrollMagic.min.js"></script>
+<script src="js/animation.velocity.min.js"></script>
+
+<!-- ...end JS Script -->
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Laravel - Shop</title>
+
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+    <link rel="stylesheet" type="text/css" href="css/crumina-fonts.css">
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="css/grid.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+
+    <!--Plugins styles-->
+
+    <link rel="stylesheet" type="text/css" href="css/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" type="text/css" href="css/swiper.min.css">
+    <link rel="stylesheet" type="text/css" href="css/primary-menu.css">
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+    <!--Styles for RTL-->
+
+    <!--<link rel="stylesheet" type="text/css" href="css/rtl.css">-->
+
+    <!--External fonts-->
+
+    <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+
+</head>
+
+
+<body class=" ">
+
+<header class="header" id="site-header">
+
+    <div class="container">
+
+        <div class="header-content-wrapper">
+
+            <ul class="nav-add">
+                <li class="cart">
+
+                    <a href="#" class="js-cart-animate">
+                        <i class="seoicon-basket"></i>
+                        <span class="cart-count">0</span>
+                    </a>
+
+                    <div class="cart-popup-wrap">
+                        <div class="popup-cart">
+                            <h4 class="title-cart">No products in the cart!</h4>
+                            <p class="subtitle">Please make your choice.</p>
+                            <div class="btn btn-small btn--dark">
+                                <span class="text">view all catalog</span>
+                            </div>
+                        </div>
+                    </div>
+
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</header>
