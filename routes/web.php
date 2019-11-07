f6b3c1b47d35b7cd5da74be8fe6823248642d030
jordanye0 <jordanyeo.work@gmail.com>
2019-11-07 02:08:58 +0100

    added route for frontcontroller

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'FrontController@index')->name('home');
+Route::resource('/products', 'ProductsController');
 
 Auth::routes();
 
-Route::resource('products', 'ProductsController');
