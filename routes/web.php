00e3a9695312051ca0763650a64369deca430d37
jordanye0 <jordanyeo.work@gmail.com>
2019-11-07 02:38:54 +0100

    route for single product

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 Route::get('/', 'FrontController@index')->name('home');
+Route::get('/product/{product}', 'FrontController@show')->name('show');
 Route::resource('/products', 'ProductsController');
 
 Auth::routes();
-
